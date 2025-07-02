f892971bf98ec73d5b1153e6616dc6ae81c6728a
mubashir khan <kiraniqbal310@gmail.com>
2025-07-02 05:00:08 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+    <p>{count}</p>
+      <button onClick={()=>{setCount(count + 1)}}>increment</button>
+        <button onClick={()=>{setCount(count - 1)}}>decrement</button>
+    </>
+  )
+}
+
+export default App
